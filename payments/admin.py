f6bf95fcc2affc065afe1ae39a202da683b9ecf3
Petro Abramov <sebshe3@gmail.com>
2023-08-06 21:57:05 +0300

    Added payments to admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Payment, PaymentAdmin)
