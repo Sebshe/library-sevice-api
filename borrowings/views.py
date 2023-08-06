b1000e2299834a544b5443dd2d1d2720e9e6a65b
Petro Abramov <sebshe3@gmail.com>
2023-08-06 22:11:53 +0300

    Added method return book in BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,8 +1,11 @@
+from datetime import date
+
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -10,6 +13,13 @@ from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
 from borrowings.tasks import send_telegram_message
+from library_service_api import settings
+from payments.utils import create_payment_and_stripe_session
+
+SUCCESS_URL = (
+    f"{settings.DOMAIN_URL}/payments/success?session_id={{CHECKOUT_SESSION_ID}}"
+)
+CANCEL_URL = f"{settings.DOMAIN_URL}/payments/cancel?session_id={{CHECKOUT_SESSION_ID}}"
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -39,6 +49,49 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
         else:
             return Response({"message": "Book is out of stock"}, status=400)
+
+    @action(detail=True, methods=["post"])
+    def return_book(self, request, pk=None):
+        """
+        Custom action to handle returning a borrowed book
+        """
+        borrowing = Borrowing.objects.get(id=pk)
+        if borrowing.user != self.request.user:
+            return Response({"message": "It's not your borrowing"}, status=400)
+        if borrowing.actual_return_date is None:
+            with transaction.atomic():
+                borrowing.actual_return_date = date.today()
+                borrowing.save()
+                borrowing.book.inventory += 1
+                borrowing.book.save()
+
+                _payment_type = None
+                _message = ""
+                if borrowing.actual_return_date > borrowing.extend_return_date:
+                    _payment_type = "FINE"
+                    _message = "Your borrowing was overdue. You`ll have to pay fine."
+                else:
+                    _payment_type = "PAYMENT"
+                    _message = "Thank you for the timely return of the book"
+
+                payment = create_payment_and_stripe_session(
+                    borrowing,
+                    success_url=SUCCESS_URL,
+                    cancel_url=CANCEL_URL,
+                    payment_type=_payment_type,
+                )
+                send_telegram_message.delay(
+                    f"{borrowing.user} successful payment for {borrowing.book}"
+                )
+                return Response(
+                    {
+                        "success": "The book was successfully returned.",
+                        "message": _message,
+                        "link": f"Pay here: {payment.stripe_session_url}",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+        return Response({"message": "Borrowing already returned"}, status=400)
 
     def get_queryset(self):
         queryset = super().get_queryset()
