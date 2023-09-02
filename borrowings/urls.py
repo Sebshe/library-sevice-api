0e156b05a932f783042dfde4a5311237bc67b1eb
Petro Abramov <sebshe3@gmail.com>
2023-09-03 00:11:20 +0300

    Fixed with borrowings urls

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from borrowings.views import BorrowingViewSet
@@ -5,6 +6,12 @@ from borrowings.views import BorrowingViewSet
 router = DefaultRouter()
 router.register(r"", BorrowingViewSet, basename="borrowing")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        "<int:pk>/return/",
+        BorrowingViewSet.as_view({"post": "return_book"}),
+        name="book-return",
+    ),
+] + router.urls
 
 app_name = "borrowings"
